57a3a7d44280129a83cb5ace94f350b1ff6a5690
vilasvarghese <vilas.varghese@gmail.com>
2023-09-24 19:49:26 +0530

    testng from mvn fix

--- SeleniumProject/src/test/java/SeleniumProject/SeleniumProject/MavenTest1.java
+++ SeleniumProject/src/test/java/SeleniumProject/SeleniumProject/MavenTest1.java
@@ -15,14 +15,18 @@ import junit.framework.TestCase;
 
 public class MavenTest1 extends TestCase{
 	
+	//download chrom driver from https://googlechromelabs.github.io/chrome-for-testing/
 	public String baseUrl = "https://www.google.com/";  
-	String driverPath = "D:\\chrome\\chromedriver_win32\\chromedriver.exe";  
+	String driverPath = "D:\\chrome\\chromedriver-win64\\chromedriver.exe";  
 	
-	public WebDriver driver ;   
+	public WebDriver driver;
+	
 	@Test             
-	public void test() {      
+	public void testGoogle() {      
 		// set the system property for Chrome driver      
+		System.out.println("inside testGoogle");
 		System.setProperty("webdriver.chrome.driver", driverPath);  
+		System.out.println("inside testGoogle "+driverPath);
 		// Create driver object for CHROME browser  
 		driver = new ChromeDriver();  
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);  
